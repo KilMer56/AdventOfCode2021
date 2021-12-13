330433c794f5e4a221ad5244559972ffe2a0f866
Killian Mer <killian.mer@payaut.com>
2021-12-13 22:02:48 +0100

    Day 3 - Solution 2

---
+++ 3/answer_2.py
@@ -0,0 +1,36 @@
+# Advent of code Year 2021 Star 5
+# Author : Killian Mer
+
+with open("input.txt", "r") as fp:
+    input = fp.read().splitlines()
+
+def get_metric(i, elements, isOxygen):
+    if len(elements) == 1: # End result if only 1 element
+        return int(elements[0], 2)
+
+    a = []
+    b = []
+
+    for j in range(len(elements)):
+        if int(elements[j][i]) == 1:
+            a.append(elements[j])
+        else:
+            b.append(elements[j])
+    
+    i += 1
+
+    if i == 1:
+        if len(a) >= len(b):
+            return get_metric(i, a, True) * get_metric(i, b, False)
+        else :
+            return get_metric(i, b, True) * get_metric(i, a, False)
+
+    if (len(a) == len(b) and isOxygen) or ((len(a) >= len(b)) == isOxygen): # If counts are equal or different
+        return get_metric(i, a, isOxygen)
+    else:
+        return get_metric(i, b, isOxygen)
+
+
+res = get_metric(0, input, None)
+
+print("Result : " + str(res))
